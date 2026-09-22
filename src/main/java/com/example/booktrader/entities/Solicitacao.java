e7875ba56fcc9a4f0fcf371cd82cc643f51356f7
pablonog <pablogomesnog@gmail.com>
2026-09-21 23:45:22 -0300

    Ajuste Rápido
    Defini a Solicitação como entidade e coloquei o Id com o Generate value

--- src/main/java/com/example/booktrader/entities/Solicitacao.java
+++ src/main/java/com/example/booktrader/entities/Solicitacao.java
@@ -1,9 +1,17 @@
 package com.example.booktrader.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Solicitacao {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String leitorSolicitante;
     private String ofertaDeTroca;
